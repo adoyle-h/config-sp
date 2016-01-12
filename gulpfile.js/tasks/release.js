2c9b112ab4f967946a340600e2e025c882941340
ADoyle <adoyle.h@gmail.com>
2016-01-12 21:45:43 +0800

    chore(gulp): update release tasks && add doc tasks

--- gulpfile.js/tasks/release.js
+++ gulpfile.js/tasks/release.js
@@ -67,11 +67,22 @@ module.exports = function(gulp, config, LL, args) {  // eslint-disable-line no-u
     gulp.task('release:pre', function(done) {
         var CP = LL.CP;
 
+        var rebaseDest;
+        var commit = args.c || args.commit;
+        var branch = args.branch;
+        if (commit) {
+            rebaseDest = commit;
+        } else if (branch) {
+            rebaseDest = branch;
+        } else {
+            rebaseDest = 'HEAD';
+        }
+
         var command = '\
             git add . && \
             git stash save "stash for release" && \
             git fetch --prune && \
-            git rebase origin/develop release \
+            git rebase ' + rebaseDest + ' release \
         ';
         CP.exec(command, done);
     });
@@ -173,6 +184,28 @@ module.exports = function(gulp, config, LL, args) {  // eslint-disable-line no-u
             git push --tags \
         ';
         CP.exec(command, done);
+    });
+
+    gulp.task('release:doc', ['doc:api', 'clean:gh-pages'], function(done) {
+        var command = '\
+            cp -rp doc/api/ gh-pages && \
+            cd gh-pages && \
+            git add . && \
+        ';
+        if (args.a || args.amend) {
+            command += '\
+                git commit --amend --no-edit && \
+                git push -f && \
+            ';
+        } else {
+            command += '\
+                git commit -m "update docs" && \
+                git push && \
+            ';
+        }
+        command += 'cd -';
+        LL.CP.exec(command, done);
+        LL.CP.exec(command, done);
     });
 
     gulp.task('release:code', function(done) {
